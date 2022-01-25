cdd8e6c3b9ded23d34ec230802815c63bfecae21
Daniel VanRyn <daniel.vanryn@mymail.champlain.edu>
2022-01-25 10:21:56 -0500

    Added seek controller to make a false wander and to proof of concept an easy to edit structure for adding functionality

--- AI-2022/Assets/Scripts/AIBehaviors/Seek.cs
+++ AI-2022/Assets/Scripts/AIBehaviors/Seek.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Seek : AIBehavior
 {
     [SerializeField]
-    Vector2 mTarget;
+    public Vector2 mTarget;
     
     public override Vector2 getSteerDirection()
     {
         base.getSteerDirection();
         Vector2 temp = new Vector2(mTarget.x - transform.position.x, mTarget.y - transform.position.y).normalized;
-        Debug.Log("direction: " + temp);
         return temp;
     }
 }
---
+++ AI-2022/Assets/SeekController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekController : MonoBehaviour
+{
+    [SerializeField]
+    Seek mSeek;
+    [SerializeField]
+    int mTimeMax = 20;
+    float mTimer = 0;
+
+    public void Update()
+    {
+        if(mTimer <=0.0f)
+        {
+            mTimer = mTimeMax;
+            int randX = Random.Range(-50, 50);
+            int randY = Random.Range(-50, 50);
+            mSeek.mTarget = new Vector2(randX, randY);
+        }
+        mTimer -= Time.deltaTime;
+    }
+}
